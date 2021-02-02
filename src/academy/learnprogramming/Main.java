071cd0140ffb74133a4330a2139827072bed489e
Justus Witmer <justus.witmer@gmail.com>
2021-02-02 15:40:06 -0600

    learing about floats and doubles

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,25 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	    // FLOAT
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float Min Value = " + myMinFloatValue);
+        System.out.println("Float Max Value = " + myMaxFloatValue);
+
+        // DOUBLE
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double Min Value = " + myMinDoubleValue);
+        System.out.println("Double Max Value = " + myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float myFloutValue = (float) 5.25;
+        // OR
+        float mySecondFloutValue = 5.25f;
+        double myDoubleValue = 5.25d;
+    }
+}
